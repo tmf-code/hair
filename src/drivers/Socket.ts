1f96a5c162bed21279b92cb5581d8ef0d4a4ac6c
Jae Perris <jae.perris@gmail.com>
2020-05-19 21:23:15 +0200

    Re-organise Socket methods

--- src/drivers/Socket.ts
+++ src/drivers/Socket.ts
@@ -23,26 +23,35 @@ export class Socket {
 
   private myCuts: boolean[] = [];
 
-  private socketEventHandlers = {
-    updateClientGrid: (grid: Grid) => {
-      this.grid = grid;
-    },
-    updateClientGrowth: (growthSpeed: number) => {
-      this.lengths = this.lengths.map((length) => Math.min(length + growthSpeed, 1));
-    },
-    updateClientLengths: (lengths: HairLengths) => {
-      this.lengths = lengths;
-    },
-    updateClientRotations: (rotations: number[]) => {
-      this.rotations = rotations;
-    },
-    updateClientCuts: (cuts: boolean[]) => {
-      this.lengths = this.lengths.map((length, lengthIndex) => (cuts[lengthIndex] ? 0 : length));
-    },
-  };
+  public constructor() {
+    const socket = this.createSocket();
+    this.attachSocketHandlers(socket);
+    this.createSocketEmitters(socket);
+  }
+
+  private createSocket() {
+    return process.env.NODE_ENV === 'production' ? io() : io('http://192.168.178.41:3001');
+  }
 
   private attachSocketHandlers(socket: SocketIOClient.Socket) {
-    Object.entries(this.socketEventHandlers).forEach(([name, handler]) => {
+    const socketEventHandlers = {
+      updateClientGrid: (grid: Grid) => {
+        this.grid = grid;
+      },
+      updateClientGrowth: (growthSpeed: number) => {
+        this.lengths = this.lengths.map((length) => Math.min(length + growthSpeed, 1));
+      },
+      updateClientLengths: (lengths: HairLengths) => {
+        this.lengths = lengths;
+      },
+      updateClientRotations: (rotations: number[]) => {
+        this.rotations = rotations;
+      },
+      updateClientCuts: (cuts: boolean[]) => {
+        this.lengths = this.lengths.map((length, lengthIndex) => (cuts[lengthIndex] ? 0 : length));
+      },
+    };
+    Object.entries(socketEventHandlers).forEach(([name, handler]) => {
       socket.on(name, handler);
     });
   }
@@ -56,6 +65,10 @@ export class Socket {
     }, Socket.EMIT_INTERVAL);
   }
 
+  private clientHasCut() {
+    return this.myCuts.some(Boolean);
+  }
+
   private updateServerCuts(socket: SocketIOClient.Socket) {
     socket.emit('updateServerCuts', this.myCuts);
   }
@@ -64,18 +77,9 @@ export class Socket {
     this.myCuts = this.myCuts.map(() => false);
   }
 
-  private clientHasCut() {
-    return this.myCuts.some(Boolean);
-  }
-
-  private createSocket() {
-    return process.env.NODE_ENV === 'production' ? io() : io('http://192.168.178.41:3001');
-  }
-
-  public constructor() {
-    const socket = this.createSocket();
-    this.attachSocketHandlers(socket);
-    this.createSocketEmitters(socket);
+  updateCuts(cuts: boolean[]) {
+    this.cutLengths(cuts);
+    this.addNewCuts(cuts);
   }
 
   private cutLengths(cuts: boolean[]) {
@@ -84,11 +88,6 @@ export class Socket {
 
   private addNewCuts(cuts: boolean[]) {
     this.myCuts = cuts.map((currentCut, cutIndex) => currentCut || this.myCuts[cutIndex]);
-  }
-
-  updateCuts(cuts: boolean[]) {
-    this.cutLengths(cuts);
-    this.addNewCuts(cuts);
   }
 }
 
