98a625d9625747727f4bd46a9ad2ac62dacc14d3
Jae Perris <jae.perris@gmail.com>
2020-05-19 22:31:55 +0200

    Extract razor props ito it's own type

--- src/components/Razor.tsx
+++ src/components/Razor.tsx
@@ -4,46 +4,41 @@ import React from 'react';
 import { useSpring, a } from 'react-spring/three';
 import razorSVG from './svgs/razor.svg';
 
-const Razor = forwardRef(
-  (
-    {
-      scale,
-    }: {
-      ref: any;
-      opacity: number;
-      scale: typeof a.mesh['scale'];
-      props?: any;
-    },
-    ref,
-  ) => {
-    const texture = useMemo(() => new TextureLoader().load(razorSVG), []);
-    const [mouseUp, setMouseUp] = useState(true);
-    useEffect(() => {
-      document.addEventListener('mousedown', () => {
-        setMouseUp(false);
-      });
-      document.addEventListener('touchstart', () => {
-        setMouseUp(false);
-      });
-      document.addEventListener('mouseup', () => {
-        setMouseUp(true);
-      });
-      document.addEventListener('touchend', () => {
-        setMouseUp(true);
-      });
-    }, []);
+type RazorProps = {
+  ref: any;
+  opacity: number;
+  scale: typeof a.mesh['scale'];
+  props?: any;
+};
 
-    const { factor } = useSpring({ factor: mouseUp ? 1.1 : 1 });
+const Razor = forwardRef(({ scale }: RazorProps, ref) => {
+  const texture = useMemo(() => new TextureLoader().load(razorSVG), []);
+  const [mouseUp, setMouseUp] = useState(true);
+  useEffect(() => {
+    document.addEventListener('mousedown', () => {
+      setMouseUp(false);
+    });
+    document.addEventListener('touchstart', () => {
+      setMouseUp(false);
+    });
+    document.addEventListener('mouseup', () => {
+      setMouseUp(true);
+    });
+    document.addEventListener('touchend', () => {
+      setMouseUp(true);
+    });
+  }, []);
 
-    return (
-      <a.mesh ref={ref} scale={factor.interpolate((f: number) => [scale * f, scale * f, 1])}>
-        <planeBufferGeometry attach="geometry" args={[1, 2.1]} />
-        <meshBasicMaterial attach="material" transparent opacity={mouseUp ? 0 : 1}>
-          <primitive attach="map" object={texture} />
-        </meshBasicMaterial>
-      </a.mesh>
-    );
-  },
-);
+  const { factor } = useSpring({ factor: mouseUp ? 1.1 : 1 });
+
+  return (
+    <a.mesh ref={ref} scale={factor.interpolate((f: number) => [scale * f, scale * f, 1])}>
+      <planeBufferGeometry attach="geometry" args={[1, 2.1]} />
+      <meshBasicMaterial attach="material" transparent opacity={mouseUp ? 0 : 1}>
+        <primitive attach="map" object={texture} />
+      </meshBasicMaterial>
+    </a.mesh>
+  );
+});
 
 export { Razor };
