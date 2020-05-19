b963920f81dc6dcefb8592fe8c4356c085bd52a9
Jae Perris <jae.perris@gmail.com>
2020-05-19 22:43:18 +0200

    Remove comments from easing functions

--- src/utilities/easing-functions.ts
+++ src/utilities/easing-functions.ts
@@ -1,54 +1,16 @@
 export const EasingFunctions = {
-  // no easing, no acceleration
-  linear: (t: number) => {
-    return t;
-  },
-  // accelerating from zero velocity
-  easeInQuad: (t: number) => {
-    return t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuad: (t: number) => {
-    return t * (2 - t);
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuad: (t: number) => {
-    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
-  },
-  // accelerating from zero velocity
-  easeInCubic: (t: number) => {
-    return t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutCubic: (t: number) => {
-    return --t * t * t + 1;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutCubic: (t: number) => {
-    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
-  },
-  // accelerating from zero velocity
-  easeInQuart: (t: number) => {
-    return t * t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuart: (t: number) => {
-    return 1 - --t * t * t * t;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuart: (t: number) => {
-    return t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t;
-  },
-  // accelerating from zero velocity
-  easeInQuint: (t: number) => {
-    return t * t * t * t * t;
-  },
-  // decelerating to zero velocity
-  easeOutQuint: (t: number) => {
-    return 1 + --t * t * t * t * t;
-  },
-  // acceleration until halfway, then deceleration
-  easeInOutQuint: (t: number) => {
-    return t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t;
-  },
+  linear: (t: number) => t,
+  easeInQuad: (t: number) => t * t,
+  easeOutQuad: (t: number) => t * (2 - t),
+  easeInOutQuad: (t: number) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t),
+  easeInCubic: (t: number) => t * t * t,
+  easeOutCubic: (t: number) => --t * t * t + 1,
+  easeInOutCubic: (t: number) =>
+    t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1,
+  easeInQuart: (t: number) => t * t * t * t,
+  easeOutQuart: (t: number) => 1 - --t * t * t * t,
+  easeInOutQuart: (t: number) => (t < 0.5 ? 8 * t * t * t * t : 1 - 8 * --t * t * t * t),
+  easeInQuint: (t: number) => t * t * t * t * t,
+  easeOutQuint: (t: number) => 1 + --t * t * t * t * t,
+  easeInOutQuint: (t: number) => (t < 0.5 ? 16 * t * t * t * t * t : 1 + 16 * --t * t * t * t * t),
 };
