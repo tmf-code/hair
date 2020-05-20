14d5afc2bdeb317e2b2fa5e91e1cd09780d2a60a
Jae Perris <jae.perris@gmail.com>
2020-05-20 17:48:14 +0200

    Use functions for dev and prod server creation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,14 +7,22 @@ const PORT = process.env.PORT || 3000;
 
 let server;
 
-if (process.env.NODE_ENV === 'production') {
-  server = Express()
+const makeProductionServer = () => {
+  return Express()
     .use(Express.static(path.join(appRoot, 'build')))
     .use(Express.static(appRoot))
     .use((req, res) => res.sendFile(path.join(appRoot, 'build', 'index.html')))
     .listen(PORT, () => console.log(`Production: Listening on ${PORT}`));
+};
+
+const makeDevelopmentServer = () => {
+  return Express().listen(3001, () => console.log(`Development: Node Listening on ${3001}`));
+};
+
+if (process.env.NODE_ENV === 'production') {
+  server = makeProductionServer();
 } else {
-  server = Express().listen(3001, () => console.log(`Production: Node Listening on ${3001}`));
+  server = makeDevelopmentServer();
 }
 
 createSocket(server);
