e4eb6185e1fd71c6faf8e5446403357929647d78
Jae Perris <jae.perris@gmail.com>
2020-05-21 21:30:37 +0200

    Make server test succeed if dev server running

--- tests/backend/server.test.ts
+++ tests/backend/server.test.ts
@@ -2,8 +2,15 @@ import { makeProductionServer } from '../../backend/src/server';
 import request from 'supertest';
 
 describe('Production server can be created', () => {
-  const server = makeProductionServer();
-  test('It should response the GET method', () => {
-    return request(server).get('/').expect(200);
+  test('It should response the GET method', (done) => {
+    const server = makeProductionServer()
+      .on('error', () => {
+        request('http://localhost:3000').get('/').expect(200);
+        done();
+      })
+      .on('listening', () => {
+        request(server).get('/').expect(200);
+        done();
+      });
   });
 });
