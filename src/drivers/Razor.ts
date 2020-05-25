c971d8b868ca9fab06007f70b6108ce763c04922
Jae Perris <jae.perris@gmail.com>
2020-05-25 23:36:54 +0200

    Make cutting turn with razor

--- src/drivers/Razor.ts
+++ src/drivers/Razor.ts
@@ -1,5 +1,5 @@
 import { Mouse } from './Mouse';
-import { Vector3, Mesh, Vector2, Box2, Camera } from 'three';
+import { Vector3, Mesh, Vector2, Box2, Camera, Triangle, Matrix3, Matrix4 } from 'three';
 import React from 'react';
 
 import { razorWidth, razorHeight } from '../utilities/constants';
@@ -10,7 +10,9 @@ export class Razor {
   private mouseRight = new Vector2();
   private ref: React.MutableRefObject<Mesh | undefined> | undefined;
   private razorBox = new Box2();
+  private razorTriangles: [Triangle, Triangle] = [new Triangle(), new Triangle()];
   private aspect = 1.0;
+  private rotation = 0;
 
   public updateFrame(
     ref: React.MutableRefObject<Mesh | undefined>,
@@ -21,24 +23,47 @@ export class Razor {
     this.ref = ref;
     this.aspect = aspect;
     const mousePos = mouseToWorld(mouse, camera);
-    this.updateRazorBox(mousePos);
+    this.updateRazorTriangles(mousePos);
     this.updateRazorPosition(mousePos);
     this.updateRazorRotation();
   }
 
   containsPoint([xPos, yPos]: [number, number]) {
-    return this.razorBox.containsPoint(new Vector2(xPos, yPos));
+    return this.razorTriangles.some((triangle) =>
+      triangle.containsPoint(new Vector3(xPos, yPos, 0)),
+    );
   }
 
-  private updateRazorBox(mousePos: Vector3) {
-    this.mouseLeft.set(mousePos.x - razorWidth * this.aspect, mousePos.y - razorHeight);
-    this.mouseRight.set(mousePos.x + razorWidth * this.aspect, mousePos.y + razorHeight);
-    this.razorBox.set(this.mouseLeft, this.mouseRight);
+  private updateRazorTriangles(mousePos: Vector3) {
+    const offsets = [
+      [-razorWidth * this.aspect, -razorHeight],
+      [-razorWidth * this.aspect, +razorHeight],
+      [+razorWidth * this.aspect, -razorHeight],
+      [+razorWidth * this.aspect, +razorHeight],
+    ];
+
+    const offsetVector2 = offsets.map((offset) =>
+      new Vector2().fromArray(offset).rotateAround(new Vector2(0, 0), this.rotation),
+    );
+
+    const absoluteVector2 = offsetVector2.map((vector) =>
+      vector.add(new Vector2(mousePos.x, mousePos.y)),
+    );
+
+    const absoluteVector3 = absoluteVector2.map((vector) => {
+      return new Vector3().fromArray([...vector.toArray(), 0]);
+    });
+
+    const triangleLeft = new Triangle().setFromPointsAndIndices(absoluteVector3, 0, 1, 2);
+    const triangleRight = new Triangle().setFromPointsAndIndices(absoluteVector3, 3, 1, 2);
+
+    this.razorTriangles = [triangleLeft, triangleRight];
   }
 
   private updateRazorRotation() {
+    this.rotation = Mouse.SmoothedAngle();
     if (this.ref?.current) {
-      this.ref.current.rotation.set(0, 0, Mouse.SmoothedAngle());
+      this.ref.current.rotation.set(0, 0, this.rotation);
       this.ref.current.matrixWorldNeedsUpdate = true;
     }
   }
