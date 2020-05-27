9ae9b80f374e4398a10efbd092f00a218f41aa85
abaran3 <alexik11@hotmail.com>
2020-05-27 11:55:48 +0200

    working commit

---
+++ src/components/colofon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Colofon = (): React.ReactElement => (
+  <div className="container">
+    <h1>Colofon</h1>
+    <p> colofon text </p>
+  </div>
+);
+
+export { Colofon };
