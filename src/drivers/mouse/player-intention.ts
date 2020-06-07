73259b0fe6dd1928a7c82fefb427e71560da33b1
Jae Perris <jae.perris@gmail.com>
2020-06-07 14:45:10 +0200

    Remove unused player intention file

--- src/drivers/mouse/player-intention.ts
+++
@@ -1,7 +0,0 @@
-import { MouseClickState } from './mouse-click-state';
-
-export class PlayerIntention {}
-
-// make class to hold player intention (outside)
-// set player state on 1 and 2 touches
-// set player state on mouse moving and mouse click
