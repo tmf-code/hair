5e906a1f2ffef682dd48e2cef704708e96ef4569
Jae Perris <jae.perris@gmail.com>
2020-06-10 15:55:03 +0200

    Add data recorder

---
+++ backend/src/utilities/recorder.ts
@@ -0,0 +1,49 @@
+import { writeFileSync } from 'fs';
+
+interface IPlayerLocations {}
+
+export class Recorder {
+  static startDelay = 10;
+  static recordCount = 30;
+  static samples: Record<
+    string,
+    {
+      rotation: number;
+      position: [number, number];
+    }[]
+  >[] = [];
+  static submissionCount = 0;
+
+  static addSample(
+    sample: Record<
+      string,
+      {
+        rotation: number;
+        position: [number, number];
+      }[]
+    >,
+  ) {
+    this.submissionCount++;
+    const shouldStart = this.submissionCount > this.startDelay;
+    if (!shouldStart) {
+      console.log(`Starting to record in ${this.startDelay - this.submissionCount} seconds`);
+      return;
+    }
+
+    if (this.samples.length > this.recordCount) {
+      this.saveToFile('./data.json');
+      return;
+    }
+
+    console.log('Adding sample ' + this.samples.length);
+    console.log('Saving in ' + (this.recordCount - this.samples.length));
+
+    this.samples.push(sample);
+  }
+
+  static saveToFile(filename: string) {
+    const jsonString = JSON.stringify(this.samples);
+    writeFileSync(filename, jsonString);
+    console.log('Saved');
+  }
+}
