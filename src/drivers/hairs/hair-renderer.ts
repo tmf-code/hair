a916d8063196832b04488ceb621c3aa27d8cdcff
Jae Perris <jae.perris@gmail.com>
2020-06-10 12:06:28 +0200

    Add getMatrixFromTransform to HairRenderer

--- src/drivers/hairs/hair-renderer.ts
+++ src/drivers/hairs/hair-renderer.ts
@@ -1,10 +1,18 @@
 import { hairLayer } from './../../utilities/constants';
-import { InstancedMesh } from 'three';
-import { getMatrixFromTransform } from '../../utilities/get-matrix-from-transform';
+import { InstancedMesh, Object3D } from 'three';
 
 type Tuple3<T> = [T, T, T];
 
+class NoUpdateObject3D extends Object3D {
+  constructor() {
+    super();
+    this.matrixAutoUpdate = false;
+  }
+}
+
 export class HairRenderer {
+  private static readonly transformHolder = new NoUpdateObject3D();
+
   static render(
     mesh: InstancedMesh,
     hairIndex: number,
@@ -24,9 +32,22 @@ export class HairRenderer {
     const rotation: Tuple3<number> = [0, 0, zRotation];
     const scale: Tuple3<number> = [width * hairWidthScale, length * hairLengthScale, 1];
 
-    const matrix = getMatrixFromTransform(position, rotation, scale);
+    const matrix = HairRenderer.getMatrixFromTransform(position, rotation, scale);
     mesh.setMatrixAt(hairIndex, matrix);
   }
+
+  static getMatrixFromTransform = (
+    position: [number, number, number],
+    rotation: [number, number, number],
+    scale: [number, number, number],
+  ) => {
+    HairRenderer.transformHolder.position.set(...position);
+    HairRenderer.transformHolder.rotation.set(...rotation);
+    HairRenderer.transformHolder.scale.set(...scale);
+    HairRenderer.transformHolder.updateMatrix();
+
+    return HairRenderer.transformHolder.matrix;
+  };
 
   static getHairLengthScale(aspect: number) {
     return 2 / aspect;
