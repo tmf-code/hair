366ceec04192fb57302b0a3269bd24283bd016bf
abaran3 <alexik11@hotmail.com>
2020-06-16 14:23:40 +0200

    Update colofon text

--- src/components/colofon-text.tsx
+++ src/components/colofon-text.tsx
@@ -19,34 +19,30 @@ const ColofonText = ({ roomName, url }: ColofonTextProps) => {
         <li>in collaboration with Moniker</li>
       </ul>
       <p>
-        Come Shave With Me is a collaborative web-based work that explores the limits of
-        digitally-bound technologies in mediating the bodily aspects of sociality. It is a virtual
-        skin that people can groom together, online. As such, the work proposes an outlet for social
-        grooming in a time of distancing.
+        Sheared silky smooth, maintained mildly prickly or kept rough and bushy… There are countless
+        ways you might like it. You won’t know until you try.
       </p>
       <p>
-        With public events and gatherings cut down to an unprecedented minimum around the world,
-        bodily self-care is - let’s be honest - not necessarily a top-most priority. Your arms,
-        armpits, chest, back, genitals, or legs are (usually) tucked away during video chats and
-        conferences after all. What part of it is just for others’ eyes to see and skins to feel?
-        What do we make of our physical bodies in isolation? And how do temporalities of bodily
-        processes interact with the ‘real-time’ pace of digital media?
+        Come Shave With Me is a virtual skin that people can groom together, online. The project
+        proposes an outlet for social grooming in a time of distancing.
       </p>
       <p>
-        Bodily processes are an easily overlooked constant to our subjectivity, with the help of
-        which we can reflect on a world in flux, like the current pandemic-induced transformations.
-        Most people will be able to tell you roughly how much their hair has grown since it all
-        started. But the virtual world often has neither the patience, nor the infrastructure to
-        accommodate the wants and needs of our bodies. Come Shave With Me speculates whether the
-        satisfactions enjoyed from bodily grooming could be recreated and shared in virtual spaces.
-        At the same time, it is a reminder of all the bodies sitting behind the screens.
+        With public events and gatherings cut down around the world, bodily self-care is - let’s be
+        honest - not necessarily a top-most priority. Your arms, armpits, chest, back, genitals, or
+        legs are (usually) tucked away during video chats and conferences after all. What part of it
+        is just for others’ eyes to see and skins to feel? What do we make of our physical bodies in
+        isolation?
       </p>
       <p>
-        This work is an outcome of an artistic research project funded by the Creative Industries
-        Fund NL, entitled Figureable. Figureable explores our relationships to and with our bodies
-        through embodiment in games.
+        The virtual world often has neither the patience, nor the infrastructure to accommodate the
+        wants and needs of our bodies. Come Shave With Me speculates whether the satisfactions
+        enjoyed from bodily grooming could be recreated and shared in virtual spaces.
       </p>
-      {/* <p>The project was funded by the Creative Industries Fund NL.</p> */}
+      <p>
+        This work is an outcome of Figurable, an artistic research project funded by the Creative
+        Industries Fund NL. Figureable explores our relationships to and with our bodies through
+        embodiment in games.
+      </p>
       <img
         className="logo-image"
         src={'/fund-logo-3.png'}
