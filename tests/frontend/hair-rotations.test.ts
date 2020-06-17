66bde9236800d6750566fa1c37d2098fa99e057f
Jae Perris <jae.perris@gmail.com>
2020-06-17 18:20:48 +0200

    Make hair rotation tests pass

--- tests/frontend/hair-rotations.test.ts
+++ tests/frontend/hair-rotations.test.ts
@@ -15,17 +15,4 @@ describe('HairRotations actions', () => {
     hairRotations.setInitialRotations(initialRotations);
     expect(hairRotations.getRotations()).toStrictEqual(initialRotations);
   });
-
-  test('Adding offset rotations updates rotations', () => {
-    const hairRotations = new HairRotations(4);
-    const initialRotations = [1, 2, 3, 4];
-    const offsets = [1, 2, 3, 4];
-
-    hairRotations.setInitialRotations(initialRotations);
-    hairRotations.setRotationOffsets(offsets);
-    expect(hairRotations.getRotations()).toStrictEqual(sumArrays(initialRotations, offsets));
-  });
 });
-
-const sumArrays = (arrayA: number[], arrayB: number[]) =>
-  arrayA.map((value: number, index: number) => value + arrayB[index]);
