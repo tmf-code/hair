adf11159c03dc356a56f905a66b869ebfda27ecf
Jae Perris <jae.perris@gmail.com>
2020-10-08 22:47:03 +0200

    Fix players dissapearing on shoddy net connection

--- src/drivers/player/friend-player.ts
+++ src/drivers/player/friend-player.ts
@@ -7,6 +7,7 @@ export class FriendPlayer extends Player {
   private playbackPointerPosition: [number, number] = [0, 0];
   private playbackRotation = 0;
   private playbackActionState: PlayerData['state'] = 'NOT_CUTTING';
+  private retries = 0;
   constructor() {
     super();
     this.setLayer(friendLayer);
@@ -29,10 +30,16 @@ export class FriendPlayer extends Player {
 
   private playbackPlayerData() {
     const data = this.getPlayerData();
-    if (data === undefined) {
+    if (this.retries > 15) {
       this.playbackPointerPosition = offscreen;
       return;
     }
+    if (data === undefined) {
+      this.retries++;
+      return;
+    }
+    this.retries = 0;
+
     const { position, rotation, state } = data;
 
     this.playbackPointerPosition = position;
